5b104a5d80302ae53b360e363c8cab258fcc3f11
paarziwal <111170192+paarziwal@users.noreply.github.com>
2024-03-06 15:12:28 +0530

    Add files via upload

---
+++ Day13/src/main/java/com/educonnect/prajwal/EduconnectApplication.java
@@ -0,0 +1,14 @@
+package com.educonnect.prajwal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduconnectApplication.class, args);
+	}
+
+	
+}
