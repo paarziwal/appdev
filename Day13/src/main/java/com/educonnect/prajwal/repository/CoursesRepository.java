b2ca7cee0ae3280e903ca91f14cb433f5274f5d9
paarziwal <111170192+paarziwal@users.noreply.github.com>
2024-04-05 23:57:33 +0530

    Delete Day13 directory

--- Day13/src/main/java/com/educonnect/prajwal/repository/CoursesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.educonnect.prajwal.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.educonnect.prajwal.model.Courses;
-
-public interface CoursesRepository extends JpaRepository<Courses, Integer>{
-
-}
