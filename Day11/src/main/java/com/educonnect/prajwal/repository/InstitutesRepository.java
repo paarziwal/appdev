0112180dac223caebc3b3dcebe951449d5752eae
paarziwal <111170192+paarziwal@users.noreply.github.com>
2024-04-05 23:43:21 +0530

    Delete Day11 directory

--- Day11/src/main/java/com/educonnect/prajwal/repository/InstitutesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.educonnect.prajwal.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.educonnect.prajwal.model.Institutes;
-
-public interface InstitutesRepository extends JpaRepository<Institutes, Integer> {
-
-}
